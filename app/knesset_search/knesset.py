4121db61f0bb64bc52d1da587159aea6f1d8403e
isragal <israeligal2@gmail.com>
2020-12-12 17:13:51 +0200

    add persion and position

--- app/knesset_search/knesset.py
+++ app/knesset_search/knesset.py
@@ -1,6 +1,7 @@
 import json
 
-from knesset_search import odata_request_builder
+from knesset_search import bills_request_builder
+from knesset_search.person_request_builder import PersonRequestBuilder
 
 
 def parse_bills(params):
@@ -15,6 +16,25 @@ def parse_bills(params):
         raise Exception("no no bills params")
 
 
+def get_persons_and_positions_for_bills(bills):
+    bills_initiators = [bill['KNS_BillInitiators'] for bill in bills]
+
+    request_builder = PersonRequestBuilder()
+
+    person_ids = set()
+    for initiators in bills_initiators:
+        for initiator in initiators:
+            person_ids.add(initiator['PersonID'])
+
+    request_builder.with_ids(person_ids)
+
+    request = request_builder.build()
+
+    response = request.execute()
+
+    return response['value']
+
+
 def knesset_handler(event, context):
     params = event.get('queryStringParameters')
     bills = parse_bills(params)
@@ -22,11 +42,25 @@ def knesset_handler(event, context):
     states = bills.get('states')
     knesset_num = bills.get('knessetNum')
 
-    request_builder = odata_request_builder.KnessetBillsRequestBuilder()
+    bills = get_bills(knesset_num, states)
+
+    add_bills_persons_and_factions(bills)
+
+    print(f"got response {bills}")
+
+    return {
+        'event': event,
+        'params': params,
+        'bills': bills,
+        'states': states
+    }
+
+
+def get_bills(knesset_num, states):
+    request_builder = bills_request_builder.BillsRequestBuilder()
 
     if states:
         request_builder.with_states(states)
-
     if knesset_num:
         request_builder.with_knesset_num(knesset_num)
 
@@ -34,15 +68,32 @@ def knesset_handler(event, context):
 
     response = bills_request.top(6).execute()
 
-    print(f"got response {response['value']}")
-
-    return {
-        'event': event,
-        'params': params,
-        'bills': bills,
-        'states': states,
-        'values': response['value']
-    }
+    return response['value']
 
 
-knesset_handler({ 'queryStringParameters': { "bills": '{ "states": [108, 109, 111, 141, 167], "knessetNum": 23 }' } }, None)
+def add_bills_persons_and_factions(bills):
+    persons_and_positions = get_persons_and_positions_for_bills(bills)
+
+    for bill in bills:
+        _add_person_and_faction(persons_and_positions, bill)
+
+
+def _add_person_and_faction(persons_with_positions, bill):
+    for initiator in bill['KNS_BillInitiators']:
+        person = next((person for person in persons_with_positions if initiator['PersonID'] == person['PersonID']),
+                      {})
+
+        positions = filter(lambda position: position['KnessetNum'] == bill['KnessetNum'],
+                           person['KNS_PersonToPositions'])
+
+        position = next((position for position in positions if position['FactionName']), {})
+
+        initiator['first_name'] = person['FirstName']
+        initiator['last_name'] = person['LastName']
+        initiator['gender'] = person['GenderDesc']
+        initiator['email'] = person['Email']
+        initiator['faction'] = position['FactionName']
+        initiator['faction_id'] = position['FactionID']
+
+
+# knesset_handler({'queryStringParameters': {"bills": '{ "states": [108, 109, 111, 141, 167], "knessetNum": 23 }'}}, None)
